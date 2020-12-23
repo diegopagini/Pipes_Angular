e7e6ad127bbb0779855dbd23c6e82d842cf24db6
Diego Pagini <diegopagini777@gmail.com>
2020-12-23 07:49:54 -0300

    async ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,24 @@ export class AppComponent {
   arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   PI: number = Math.PI;
+
+  Porcentaje: number = 0.234;
+
+  salario: number = 1234.5;
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: 20,
+    },
+  };
+
+  valorPromesa = new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve('llego la data');
+    }, 4500);
+  });
 }
