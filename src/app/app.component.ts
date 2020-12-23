2c450052db75326f481cbd70b4bc762a82d3489d
Diego Pagini <diegopagini777@gmail.com>
2020-12-23 07:31:16 -0300

    number

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre: string = 'Capitan America';
+
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  PI: number = Math.PI;
 }
